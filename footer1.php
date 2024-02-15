4e492cc5c29022f924d29cb4bc7abf6398a0b987
mahesh70588 <138879761+mahesh70588@users.noreply.github.com>
2024-02-15 05:47:36 +0530

    Add files via upload

---
+++ footer1.php
@@ -0,0 +1,24 @@
+</div><!--end row-->
+</div><!--end container-->
+
+
+
+    
+
+
+
+
+
+
+
+
+    <!--javascript-->
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/all.min.js"></script>
+</body>
+</html>
+
+
+    
